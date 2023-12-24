41dd080f516d8a6efc438b36f3a7a8712ec397de
MateusLucasDaSilva <mateuslukas505@gmail.com>
2023-12-24 01:54:05 -0300

    removi a camada de infra

--- lib/di/injectable.config.dart
+++ lib/di/injectable.config.dart
@@ -11,8 +11,8 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../infra/clients/http/http_client.dart' as _i3;
-import '../infra/clients/http/http_client_impl.dart' as _i4;
+import '../data/clients/http/http_client.dart' as _i3;
+import '../data/clients/http/http_client_impl.dart' as _i4;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
